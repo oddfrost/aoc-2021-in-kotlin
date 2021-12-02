d2789def25e77ef30c5ff1a6205f4d5a3cb52c7e
Oleg Morozov <skill179@gmail.com>
2021-12-02 11:00:21 +0300

    day one

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,37 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        var counter = 0
+        for (i in 1 until input.size) {
+            if (input[i] > input[i-1]) counter++
+        }
+        return counter
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun sumNumbers(input: List<Int>, pos: Int, number: Int): Int {
+        var sum = 0
+        for (i in pos until (pos+number)){
+            sum += input[i]
+        }
+        return sum
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(input: List<Int>): Int {
+        var counter = 0
+        var prevSum = sumNumbers(input, 0, 3)
+        for (i in 1 until input.size - 2) {
+            val currentSum = sumNumbers(input, i, 3)
+            if (currentSum > prevSum) counter++
+            prevSum = currentSum
+        }
+        return counter
+    }
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("day01")
+    val parsed = input.map { it.toInt() }
+    println("Processing ${parsed.size} lines")
+
+    print("First part: ")
+    println(part1(parsed))
+    print("Second part: ")
+    println(part2(parsed))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -2,12 +2,18 @@ import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
 
+const val headerLen = 20
+
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInput(name: String) = File("input", "$name.txt").readLines()
 
 /**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+fun printHeader(header: String){
+
+}
